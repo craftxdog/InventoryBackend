593ff8f02a05c0c1c60a1c95ef2331fc7f2aa0ce
craftxdog <aaronmars77@gmail.com>
2024-05-26 02:53:01 -0600

    feat(done): eoq method is done
    testing new features on my models

--- src/Routes/AuthRoutes.ts
+++ src/Routes/AuthRoutes.ts
@@ -3,40 +3,47 @@ import { AuthController } from "../Controllers/AuthController";
 import { body } from "express-validator";
 import { handleInputErrors } from "../Middleware/Validations";
 
+const router = Router();
 
-const router = Router()
-
-router.post('/create-account',
-  body('userName').notEmpty().withMessage('El nombre de usuario es obligatorio'),
-  body('password').isLength({min: 8}).withMessage('La contraseña es muy corta, minimo 8 caracteres'),
-  body('password_confirmation').custom((value, { req }) => {
-    if (value !== req.body.password){
-      throw new Error ('Verifique las contraseñas')
+router.post(
+  "/create-account",
+  body("userName")
+    .notEmpty()
+    .withMessage("El nombre de usuario es obligatorio"),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("La contraseña es muy corta, minimo 8 caracteres"),
+  body("password_confirmation").custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error("Verifique las contraseñas");
     }
-    return true
+    return true;
   }),
-  body('email').isEmail().withMessage('E-mail no válido'),
+  body("email").isEmail().withMessage("E-mail no válido"),
   handleInputErrors,
-  AuthController.createAccount
-)
+  AuthController.createAccount,
+);
 
-router.post('/confirm-account',
-  body('token').notEmpty().withMessage('El Token no puede estar vacio'),
+router.post(
+  "/confirm-account",
+  body("token").notEmpty().withMessage("El Token no puede estar vacio"),
   handleInputErrors,
-  AuthController.confirmAccount
-)
+  AuthController.confirmAccount,
+);
 
-router.post('/login',
-  body('email').isEmail().withMessage('E-mail no válido'),
-  body('password').notEmpty().withMessage('El password no puede estar vacio'),
+router.post(
+  "/login",
+  body("email").isEmail().withMessage("E-mail no válido"),
+  body("password").notEmpty().withMessage("El password no puede estar vacio"),
   handleInputErrors,
-  AuthController.login
-)
+  AuthController.login,
+);
 
-router.post('/request-code',
-  body('email').isEmail().withMessage('E-mail no válido'),
+router.post(
+  "/request-code",
+  body("email").isEmail().withMessage("E-mail no válido"),
   handleInputErrors,
-  AuthController.requestConfirmationCode
-)
+  AuthController.requestConfirmationCode,
+);
 
-export default router
+export default router;
