9b585eb0d6f4477107ea2ce814d674bed8803322
craftxdog <aaronmars77@gmail.com>
2024-06-12 13:44:04 -0600

    Working in to LUC Method

--- src/Controllers/MethodLUCController.ts
+++ src/Controllers/MethodLUCController.ts
@@ -5,51 +5,6 @@ import MethodLUC from "../Models/MethodLUC";
 export class MethodLUCController {
   static createMethodLUC = async (req: Request, res: Response) => {
     try {
-      const { semanas, S, K, LT, requerimientoBruto } = req.body;
-
-      let unidades = 0;
-      let costoTotal = S + K;
-      let k = K;
-      const resultados = [];
-
-      let periodos = "";
-      let inicioCiclo = 0;
-
-      for (let i = 1; i <= semanas; i++) {
-        if ((i - 1) % 5 === 0) {
-          inicioCiclo = i - 1;
-          unidades = requerimientoBruto[inicioCiclo];
-          periodos = `${i}`;
-        } else {
-          unidades += requerimientoBruto[i - 1];
-          periodos += `-${i}`;
-        }
-        console.log(periodos, unidades);
-      }
-
-      /*
-      const costoUnitarioTotal = costoTotal / unidades;
-      const requerimientoBruto = 0;
-      const recepcionPlaneada = i >= LT ? unidades : 0;
-
-      const resultado = {
-        periodo: i,
-        unidades,
-        S,
-        K: k,
-        costoTotal,
-        costoUnitarioTotal,
-        requerimientoBruto,
-        recepcionPlaneada,
-      };
-
-      unidades++;
-      resultados.push(resultado);
-      const methodLuC = new MethodLUC(resultado);
-      methodLuC.methods = req.methods._id;
-      req.methods.methodLUC.push(methodLuC._id);
-      await Promise.allSettled([methodLuC.save(), req.methods.save()]);
-*/
       res.status(201).send("Método LUC Cáculado.");
     } catch (error) {
       console.error(colors.dim.bold(`Error al Cácular, ${error}`));
